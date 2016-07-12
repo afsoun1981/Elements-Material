957681ac84125c9ca70c1e7ad7b6cc47ad022861
Afsoun Amiri <afsoun1981@gmail.com>
2016-07-12 23:13:29 +0200

    menu component implementation and styling finished.

--- src/app/components/el-menu/el-menu.component.ts
+++ src/app/components/el-menu/el-menu.component.ts
@@ -1,21 +1,43 @@
 /// <reference path="../../../typings.d.ts" />
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { MdButton } from '@angular2-material/button';
+import { MdIcon } from '@angular2-material/icon';
+
+export class MenuItem {
+  icon: string;
+  title: string;
+  subtitle: string;
+}
+
+export class MenuClickEvent {
+  item: MenuItem;
+  event: Event;
+}
 
 @Component({
   moduleId: module.id,
   selector: 'el-menu',
   templateUrl: 'el-menu.component.html',
   styleUrls: ['el-menu.component.css'],
-  directives: [ MdButton ]
+  directives: [ MdButton, MdIcon ]
 })
 export class ElMenuComponent implements OnInit {
   @Input() title: string;
-  @Input() items : any[];
+  @Input() items : MenuItem[];
+
+  @Output()
+  private menuClick = new EventEmitter();
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  menuClicked(item : MenuItem, event : Event) {
+    var menuItemEvent : MenuClickEvent = { 
+      item: item, 
+      event: event 
+    };
+    this.menuClick.emit(menuItemEvent);
+  }
 }
