0e430f313ee5be6b8c6df55dc502e0316d8391a1
afsoun <afsoun1981@gmail.com>
2016-09-01 14:43:19 +0200

    added data to el-mini-erwin with style

--- src/app/components/el-mini-erwin/el-mini-erwin.component.ts
+++ src/app/components/el-mini-erwin/el-mini-erwin.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MdButton } from '@angular2-material/button';
+import { MdIcon } from '@angular2-material/icon';
+
+export class MiniErwinIcon {
+  icon: string;
+}
 
 @Component({
   moduleId: module.id,
   selector: 'el-mini-erwin',
   templateUrl: 'el-mini-erwin.component.html',
-  styleUrls: ['el-mini-erwin.component.css']
+  styleUrls: ['el-mini-erwin.component.css'],
+  directives: [ MdButton, MdIcon ]
 })
 export class ElMiniErwinComponent implements OnInit {
+  @Input() title: string;
+  @Input() icons : MiniErwinIcon[];
 
   constructor() {}
 
   ngOnInit() {
+  	this.title = "Artikel";
+		this.icons = [
+		  { icon: "reorder"},
+		  { icon: "shopping_basket"},
+		  { icon: "reorder"},
+		  { icon: "perm_media"},
+		  { icon: "reorder"},
+		  { icon: "shopping_cart"}
+		];
   }
-
 }
